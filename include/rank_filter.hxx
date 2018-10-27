2748c7628794680e17e9324adc3b373d8c612530
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-10-26 22:37:43 -0400

    Ensure order of repeated values in the `set`
    Instead of using a `multiset`, switch to a `set`. To ensure that repeat
    values can be handled, use a `pair` to tie each value to a `size_t`
    number that indicates how many values have been seen thus far. Not only
    does the `size_t` value break ties between duplicate values (and ensure
    each `pair` is unique), it additionally ensures that repeat values
    maintain their order when inserted into the `set`. This property is
    relied on by the rank filter algorithm here to handle special insertion
    cases. While C++11's `multiset` and Boost's `multiset` have this
    property, we are not sure of the latter and cannot find documentation to
    back it up. Thus creating `pair`s that avoids any need to rely on
    `multiset`s that may or may not maintain ordering of duplicate values.

--- include/rank_filter.hxx
+++ include/rank_filter.hxx
@@ -7,6 +7,7 @@
 #include <functional>
 #include <iostream>
 #include <iterator>
+#include <utility>
 
 #include <boost/array.hpp>
 #include <boost/container/set.hpp>
@@ -34,11 +35,12 @@ inline void lineRankOrderFilter1D(const I1& src_begin, const I1& src_end,
     typedef T1 T;
 
     // Define container types that will be used.
-    typedef boost::container::multiset< T,
-            std::less<T>,
-            boost::container::node_allocator<T>,
-            boost::container::tree_assoc_options< boost::container::tree_type<boost::container::scapegoat_tree> >::type> multiset;
-    typedef std::deque< typename multiset::iterator > deque;
+    typedef std::pair<T, size_t> TC;
+    typedef boost::container::set< TC,
+            std::less<TC>,
+            boost::container::node_allocator<TC>,
+            boost::container::tree_assoc_options< boost::container::tree_type<boost::container::scapegoat_tree> >::type> set;
+    typedef std::deque< typename set::iterator > deque;
 
     // Precompute some window lengths.
     const size_t half_length_add_1 = half_length + 1;
@@ -55,7 +57,8 @@ inline void lineRankOrderFilter1D(const I1& src_begin, const I1& src_end,
     assert((0 <= rank) && (rank <= 1));
 
     // Track values in window both in sorted and sequential order.
-    multiset sorted_window;
+    size_t values_seen = 0;
+    set sorted_window;
     deque window_iters(length);
 
     // Iterators in source and destination.
@@ -63,8 +66,8 @@ inline void lineRankOrderFilter1D(const I1& src_begin, const I1& src_end,
     I1 dest_pos = dest_begin;
 
     // Get the initial window in sequential order with reflection.
-    // Insert elements in order to the multiset for sorting.
-    // Store all multiset iterators into the deque in sequential order.
+    // Insert elements in order to the set for sorting.
+    // Store all set iterators into the deque in sequential order.
     {
         std::deque<T> window_init(half_length_add_1);
 
@@ -75,35 +78,41 @@ inline void lineRankOrderFilter1D(const I1& src_begin, const I1& src_end,
         }
         ++src_pos;
 
-        for (size_t j = 0; j < half_length; ++j)
+        TC each_pair;
+        for (size_t j = 0; j < half_length; ++j, ++values_seen)
         {
-            window_iters[j] = sorted_window.insert(window_init[j]);
+            each_pair.first = window_init[j];
+            each_pair.second = values_seen;
+            window_iters[j] = sorted_window.insert(each_pair).first;
         }
-        for (size_t j = half_length; j < length; ++j)
+        for (size_t j = half_length; j < length; ++j, ++values_seen)
         {
-            window_iters[j] = sorted_window.insert(window_init.back());
+            each_pair.first = window_init.back();
+            each_pair.second = values_seen;
+            window_iters[j] = sorted_window.insert(each_pair).first;
             window_init.pop_back();
         }
     }
 
     // Window position corresponding to this rank.
     const size_t rank_pos = static_cast<size_t>(boost::math::round(rank * length_sub_1));
-    typename multiset::iterator rank_point = sorted_window.begin();
+    typename set::iterator rank_point = sorted_window.begin();
     std::advance(rank_point, rank_pos);
 
     // Roll window forward one value at a time.
-    typename multiset::iterator prev_iter;
+    typename set::iterator prev_iter;
     T prev_value;
-    T rank_value = *rank_point;
+    T rank_value = rank_point->first;
     *dest_pos = rank_value;
     T next_value;
+    TC next_pair;
     size_t window_reflect_pos = length_sub_1;
     bool src_not_empty = ( src_pos != src_end );
     while ( src_not_empty || ( window_reflect_pos > 0 ) )
     {
         // Get last value to remove from window.
         prev_iter = window_iters.front();
-        prev_value = *prev_iter;
+        prev_value = prev_iter->first;
         window_iters.pop_front();
 
         // Determine next value to add to window.
@@ -117,8 +126,10 @@ inline void lineRankOrderFilter1D(const I1& src_begin, const I1& src_end,
         else
         {
             window_reflect_pos -= 2;
-            next_value = *(window_iters[window_reflect_pos]);
+            next_value = window_iters[window_reflect_pos]->first;
         }
+        next_pair.first = next_value;
+        next_pair.second = ++values_seen;
 
         // Remove old value and add new value to the window.
         // Handle special cases where `rank_pos` may have an adjusted position
@@ -126,46 +137,46 @@ inline void lineRankOrderFilter1D(const I1& src_begin, const I1& src_end,
         if ( ( rank_value < prev_value ) && ( rank_value <= next_value ) )
         {
             sorted_window.erase(prev_iter);
-            window_iters.push_back(sorted_window.insert(next_value));
+            window_iters.push_back(sorted_window.insert(next_pair).first);
         }
         else if ( ( rank_value >= prev_value ) && ( rank_value > next_value ) )
         {
             if ( rank_point == prev_iter )
             {
-                window_iters.push_back(sorted_window.insert(next_value));
+                window_iters.push_back(sorted_window.insert(next_pair).first);
                 --rank_point;
                 sorted_window.erase(prev_iter);
             }
             else
             {
                 sorted_window.erase(prev_iter);
-                window_iters.push_back(sorted_window.insert(next_value));
+                window_iters.push_back(sorted_window.insert(next_pair).first);
             }
         }
         else if ( ( rank_value < prev_value ) && ( rank_value > next_value ) )
         {
             sorted_window.erase(prev_iter);
-            window_iters.push_back(sorted_window.insert(next_value));
+            window_iters.push_back(sorted_window.insert(next_pair).first);
             --rank_point;
         }
         else if ( ( rank_value >= prev_value ) && ( rank_value <= next_value ) )
         {
             if (rank_point == prev_iter)
             {
-                window_iters.push_back(sorted_window.insert(next_value));
+                window_iters.push_back(sorted_window.insert(next_pair).first);
                 ++rank_point;
                 sorted_window.erase(prev_iter);
             }
             else
             {
                 sorted_window.erase(prev_iter);
-                window_iters.push_back(sorted_window.insert(next_value));
+                window_iters.push_back(sorted_window.insert(next_pair).first);
                 ++rank_point;
             }
         }
 
         // Store rank value in the destination.
-        *(++dest_pos) = rank_value = *rank_point;
+        *(++dest_pos) = rank_value = (rank_point->first);
     }
 }
 
